ad7de96c15b9608d12b5726568f03555a6407142
Jorge Sanchez <sandix390@gmail.com>
2021-09-08 20:18:36 -0500

    solve first-partial

--- challenges/first-partial/go-functions-methods/lab.mk
+++ challenges/first-partial/go-functions-methods/lab.mk
@@ -7,15 +7,15 @@ build:
 
 test: build
 	@echo Test 1 - 2 points
-	./${APP_NAME} 2 | true
+	./${APP_NAME} 2 || true
 	@echo Test 2 - 4 point
-	./${APP_NAME} 4 | true
+	./${APP_NAME} 4 || true
 	@echo Test 3 - 8 points
-	./${APP_NAME} 8 | true
+	./${APP_NAME} 8 || true
 	@echo Test 4 - 12 points
-	./${APP_NAME} 12 | true
+	./${APP_NAME} 12 || true
 	@echo Test 5 - no points
-	./${APP_NAME} | true
+	./${APP_NAME} || true
 	make clean
 
 clean:
