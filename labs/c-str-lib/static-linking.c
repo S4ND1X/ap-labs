81c10799079d0c661618fd05a1faa914f4639356
Jorge Sanchez <sandix390@gmail.com>
2021-09-30 19:26:21 -0500

    solve str-lib

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+extern char *mystradd(char *, char *);
+extern int mystrlen(char *);
+extern int mystrfind(char *, char *);
+
+int main(int argc, char *argv[])
+{
+    if (argc == 4 && strcmp(argv[1], "-add") == 0)
+    {
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char *new_string = mystradd(argv[2], argv[3]);
+        printf("New String          : %s\n", new_string);
+        printf("New length          : %d\n", mystrlen(new_string));
+        return 0;
+    }
+
+    if (argc == 4 && strcmp(argv[1], "-find") == 0)
+    {
+        int find = mystrfind(argv[2], argv[3]);
+        if (find > -1)
+        {
+            printf("['%s'] string was found at [%d] position\n", argv[3], find);
+        }
+        else
+        {
+            printf("No ocurrences found");
+        }
+        return 0;
+    }
+    printf("Bad usage, please provide valid argument(s)\n");
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,50 @@
 
 
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str)
+{
+    int len = 0;
+    while (*str != '\0')
+    {
+        len++;
+        str++;
+    }
+    return len;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition)
+{
+    char *result = origin + mystrlen(origin);
+    while (*addition != '\0')
+    {
+        *result = *addition;
+        result++;
+        addition++;
+    }
+    *result = '\0';
+    return origin;
 }
+ 
+int mystrfind(char *origin, char *substr)
+{
+    int len = 0;
+    int len2 = 0;
+    int verif = mystrlen(substr);
+    int tmp = 0;
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+    while (origin[len] != '\0')
+    {
+        tmp = len;
+        while (origin[len] == substr[len2] && origin[len] != '\0')
+        {
+            len++;
+            len2++;
+        }
+        if (len2 == verif)
+        {
+            return tmp;
+        }
+        len++;
+        len2 = 0;
+    }
+    return -1;
 }
