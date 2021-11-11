58c2246c0386126f4b7361bf9e020411304848f0
Jorge Sanchez <sandix390@gmail.com>
2021-11-10 22:34:15 -0600

    solve lab

--- labs/c-file-dir-monitor/logger.c
+++ labs/c-file-dir-monitor/logger.c
@@ -1,23 +1,134 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
 #include "logger.h"
 
-int initLogger(char *logType) {
+#define RESET 0
+#define colorFondoNegro 40
+
+void setColor(int attr, int fg, int bg);
+void setDefaultColor();
+
+void setColor(int attr, int fg, int bg)
+{
+    char command[13];
+    sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg, bg);
+    printf("%s", command);
+}
+
+void setDefaultColor()
+{
+    char command[13];
+    sprintf(command, "%c[%d;0;0m", 0x1B, RESET);
+    printf("%s", command);
+}
+
+int isSyslog = 0;
+
+int initLogger(char *logType)
+{
     printf("Initializing Logger on: %s\n", logType);
+    if (strcmp(logType, "syslog") == 0)
+    {
+        isSyslog = 1;
+    }
+    else if (strcmp(logType, "stdout") == 0 || strlen(logType) == 0)
+    {
+        isSyslog = 0;
+    }
+    else
+    {
+        printf("ERROR: Invalid arguments");
+    }
+
     return 0;
 }
 
-int infof(const char *format, ...) {
+int infof(const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+    setColor(RESET, 36, colorFondoNegro); //color
+
+    if (isSyslog == 1)
+    {
+        vsyslog(LOG_INFO, format, arg);
+        closelog();
+    }
+    else if (isSyslog == 0)
+    {
+        vprintf(format, arg);
+    }
+
+    va_end(arg);
+    setDefaultColor();
     return 0;
 }
 
-int warnf(const char *format, ...) {
+int warnf(const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+    setColor(RESET, 35, colorFondoNegro); //color
+
+    if (isSyslog == 1)
+    {
+        vsyslog(LOG_INFO, format, arg);
+        closelog();
+    }
+    else if (isSyslog == 0)
+    {
+        vprintf(format, arg);
+    }
+
+    va_end(arg);
+    setDefaultColor();
+    printf("\n");
     return 0;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
+int errorf(const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+    setColor(RESET, 31, colorFondoNegro); //color
+
+    if (isSyslog == 1)
+    {
+        vsyslog(LOG_INFO, format, arg);
+        closelog();
+    }
+    else if (isSyslog == 0)
+    {
+        vprintf(format, arg);
+    }
+
+    va_end(arg);
+    setDefaultColor();
+    printf("\n");
     return 0;
 }
 
+int panicf(const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+    setColor(RESET, 34, colorFondoNegro); //color
+
+    if (isSyslog == 1)
+    {
+        vsyslog(LOG_INFO, format, arg);
+        closelog();
+    }
+    else if (isSyslog == 0)
+    {
+        vprintf(format, arg);
+    }
+
+    va_end(arg);
+    setDefaultColor();
+    printf("\n");
+    return 0;
+}
--- labs/c-file-dir-monitor/monitor.c
+++ labs/c-file-dir-monitor/monitor.c
@@ -1,7 +1,113 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+#include <unistd.h>
 #include "logger.h"
 
-int main(){
-    // Place your magic here
-    return 0;
+#define MAX_EVENT_MONITOR 2048
+#define NAME_LEN 32
+#define MONITOR_EVENT_SIZE (sizeof(struct inotify_event))
+#define BUFFER_LEN MAX_EVENT_MONITOR *(MONITOR_EVENT_SIZE + NAME_LEN)
+
+int main(int argc, char **argv)
+{
+
+    if (argc < 2)
+    {
+        errorf("ERROR: Too few arguments");
+        exit(2);
+    }
+    else
+    {
+
+        const char *path = argv[1];
+        int fd, watch_desc;
+        char buffer[BUFFER_LEN];
+        fd = inotify_init();
+
+        if (fd < 0)
+        {
+            perror("ERROR: Notify has an error");
+        }
+
+        watch_desc = inotify_add_watch(fd, path, IN_CREATE | IN_MOVED_FROM | IN_MOVED_TO | IN_DELETE);
+
+        if (watch_desc == -1)
+        {
+            warnf("Warning: Couldn't add watch to %s \n", path);
+        }
+        else
+        {
+            infof("Starting File/Directory Monitor on %s \n", path);
+            printf("\n");
+        }
+
+        int i = 0;
+
+        while (1)
+        {
+            i = 0;
+            int total_read = read(fd, buffer, BUFFER_LEN);
+
+            if (total_read < 0)
+            {
+                perror("Error: read error");
+            }
+
+            while (i < total_read)
+            {
+                struct inotify_event *event = (struct inotify_event *)&buffer[i];
+
+                if (event->len)
+                {
+                    if (event->mask & IN_CREATE)
+                    {
+                        if (event->mask & IN_ISDIR)
+                        {
+                            infof("- [Directory - Create]: %s \n", event->name);
+                        }
+                        else
+                        {
+                            infof("- [File Create]: %s \n", event->name);
+                        }
+                    }
+                    else if (event->mask & IN_DELETE)
+                    {
+                        if (event->mask & IN_ISDIR)
+                        {
+                            infof("- [Directory - Removal]: %s\n", event->name);
+                        }
+                        else
+                        {
+                            infof("- [File removal]: %s\n", event->name);
+                        }
+                    }
+                    else if (event->mask & IN_MOVED_FROM)
+                    {
+                        if (event->mask & IN_ISDIR)
+                        {
+                            infof("- [Directory - Rename]: %s", event->name);
+                        }
+                        else
+                        {
+                            infof("- [File rename]: %s", event->name);
+                        }
+                    }
+                    else if (event->mask & IN_MOVED_TO)
+                    {
+                        if (!(event->mask & IN_ISDIR))
+                        {
+                            infof(" -> %s \n", event->name);
+                        }
+                    }
+
+                    i += MONITOR_EVENT_SIZE + event->len;
+                }
+            }
+        }
+
+        inotify_rm_watch(fd, watch_desc);
+        close(fd);
+        return (EXIT_SUCCESS);
+    }
 }
